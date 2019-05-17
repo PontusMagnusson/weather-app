499692b93e05ec09ccbafd27fef61934ab1b6062
MAGNUSSON Pontus <pontus.magnusson@protonmail.com>
2019-05-17 13:00:23 +0200

    Test possible fix for heroku complaining about api-key file

--- src/utils/getKey.js
+++ src/utils/getKey.js
@@ -2,6 +2,10 @@ const fs = require('fs')
 
 getKey = (key) => {
 
+    if(!fs.existsSync('./api-keys.json')) {
+        return
+    }
+
     const keys = JSON.parse(fs.readFileSync('./api-keys.json'))
 
     return keys[key]
